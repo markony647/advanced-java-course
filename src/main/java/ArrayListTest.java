2b796f0e9f37c9c594005dbc09a99db369893cbc
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-05-15 12:43:47 +0300

    Add ArrayList vs LinkedList measurements example

---
+++ src/main/java/ArrayListTest.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        System.out.println(arrayList);
+    }
+}
---
+++ src/main/java/LinkedListTest.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListTest {
+
+    public static void main(String[] args) {
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        measureTime(linkedList);
+        System.out.println("===========");
+        measureTime(arrayList);
+    }
+
+    private static void measureTime(List<Integer> listToMeasure) {
+        String listType;
+        if (listToMeasure instanceof LinkedList) {
+            listType = "[LinkedList]";
+        } else {
+            listType = "[ArrayList]";
+        }
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            listToMeasure.add(i);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println(listType + "Adding elements to the end: " + (end - start));
+
+        start = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+            listToMeasure.get(i);
+        }
+        end = System.currentTimeMillis();
+
+        System.out.println(listType + "Getting elements: " + (end - start));
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            listToMeasure.add(0, i);
+        }
+
+        end = System.currentTimeMillis();
+        System.out.println(listType + "Adding elements to the beginning: " + (end - start));
+    }
+}
