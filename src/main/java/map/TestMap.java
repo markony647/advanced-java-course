40a4bd5651a9d0566dd89ae657b90876f6198fbd
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-05-18 15:23:51 +0300

    Add Map and Set examples

---
+++ src/main/java/map/TestMap.java
@@ -0,0 +1,34 @@
+package map;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TestMap {
+
+    public static void main(String[] args) {
+        Map<Integer, String> hashMap = new HashMap<>(); // order is NOT guaranteed
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>(); // order is guaranteed
+        Map<Integer, String> treeMap = new TreeMap<>(); // sorted by key (criteria for sorting should be set)
+
+        testMap(hashMap);
+        testMap(linkedHashMap);
+        testMap(treeMap);
+
+    }
+
+    public static void testMap(Map<Integer, String> map) {
+        map.put(39, "Bob");
+        map.put(32, "Sam");
+        map.put(0, "Mike");
+        map.put(78, "John");
+        map.put(1500, "Lee");
+        map.put(7, "Bob");
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println("======================");
+    }
+}
---
+++ src/main/java/set/TestSet.java
@@ -0,0 +1,61 @@
+package set;
+
+import java.util.*;
+
+public class TestSet {
+
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet<>();
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        Set<String> treeSet = new TreeSet<>();
+
+        testSet(hashSet);
+        testSet(linkedHashSet);
+        testSet(treeSet);
+
+        // Operations
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(0);
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+
+        // union
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+        System.out.println("Union: " + union);
+
+        // intersection
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println("Intersection: " + intersection);
+
+        // difference
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        System.out.println("Difference: " + difference);
+
+    }
+
+    public static void testSet(Set<String> set) {
+        set.add("Vitaly");
+        set.add("John");
+        set.add("Sam");
+        set.add("Bob");
+
+        for (String name : set) {
+            System.out.println(name);
+        }
+        System.out.println("======================");
+    }
+}
